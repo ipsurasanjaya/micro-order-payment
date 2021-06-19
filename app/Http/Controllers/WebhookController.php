d3e471624639d84c01887bf75a56deaa9ee723f4
sura sanjaya <surasanjaya@gmail.com>
2021-06-19 19:12:02 +0800

    First commit

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use App\PaymentLog;
+use Illuminate\Http\Request;
+
+class WebhookController extends Controller
+{
+    public function midtransHandler(Request $request)
+    {   
+        $data = $request->all();
+
+        $signatureKey = $data['signature_key'];
+
+        $orderId = $data['order_id'];
+        $statusCode = $data['status_code'];
+        $grossAmount = $data['gross_amount'];
+        $serverKey = env('MIDTRANS_SERVER_KEY');
+
+        $mySignatureKey = hash('sha512', $orderId.$statusCode.$grossAmount.$serverKey);
+
+        $transactionStatus = $data['transaction_status'];
+        $type = $data['payment_type'];
+
+        if ($signatureKey !== $mySignatureKey) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid signature'
+            ], 400);
+        }
+
+        $realOrderId = explode('-', $orderId);
+
+        $order = Order::find($realOrderId[0]);
+
+        if(!$order){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Order id not found'
+            ], 404);
+        }
+
+        if($order->status === 'success'){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Operation not permitted'
+            ], 405);
+        }       
+
+        if ($transactionStatus == 'settlement'){
+            // TODO set transaction status on your database to 'success'
+            $order->status = 'success';
+        } else if ($transactionStatus == 'cancel' ||
+          $transactionStatus == 'deny' ||
+          $transactionStatus == 'expire'){
+          // TODO set transaction status on your database to 'failure'
+          $order->status = 'failure';
+        } else if ($transactionStatus == 'pending'){
+            // TODO set transaction status on your database to 'pending' / waiting payment
+            $order->status = 'pending';
+        }
+
+        $logData = [
+            'status' => $transactionStatus,
+            'raw_response' => json_encode($data),
+            'order_id' => $realOrderId[0],
+            'payment_type' => $type,
+        ];
+
+        PaymentLog::create($logData);
+        $order->save();
+
+        if($order->status === 'success'){
+            createPremiumAccess([
+                'user_id' => $order->user_id,
+                'course_id' => $order->course_id,
+            ]);
+           
+        }
+
+        return response()->json('Ok');
+    }
+}
---
+++ app/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'user_id', 'course_id', 'status', 'meta_data', 'snap_url'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+        'meta_data' => 'array',
+    ];
+
+    
+}
